e613906d54e11b0294a24d87af5d96474117afb3
tfoxo <tfwroble@gmail.com>
2014-07-17 12:42:47 -0500

    general commit

---
+++ Source/genericwav/Source/Views/MainDialog.xaml.cs
@@ -0,0 +1,27 @@
+﻿/* oio * 7/7/2014 * Time: 1:13 AM
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using FirstFloor.ModernUI.Windows.Controls;
+
+namespace genericwav.Views
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class MainDialog : ModernDialog
+	{
+		
+		public MainDialog()
+		{
+			InitializeComponent();
+		}
+	}
+}
